d18f8e8ce20a4e59a765561396c0e4e777d45c15
Jack Kingsman <jack.kingsman@gmail.com>
2016-10-16 12:26:00 -0700

    Add console-logged classname assembly

--- js/classloader.js
+++ js/classloader.js
@@ -1,15 +1,21 @@
 function buildList() {
-    $.each(requirements, function(requirementgroupName, requirementGroup){
-      console.log("GROUP: " + requirementgroupName);
-      $.each(requirementGroup, function(requirementName, satisfiedBy){
-        console.log("-->" + requirementName);
-      });
+    $.each(requirements, function(requirementgroupName, requirementGroup) {
+        console.log('GROUP: ' + requirementgroupName);
+        $.each(requirementGroup, function(requirementName, satisfiedBy) {
+            var classRequirementName = 'req' + requirementName;
+            var classSatisfierNames = satisfiedBy.map(function(satisfier) {
+                return 'satisfiedBy' + satisfier;
+            });
+            classSatisfierNames.push(classRequirementName);
+            var className = classSatisfierNames.join(' ');
+            console.log(className);
+        });
     });
 }
 
 function loadRequirements() {
     $.ajax({
-        dataType: "json",
+        dataType: 'json',
         url: requirementsUrl,
         success: function(requirementsData) {
             requirements = requirementsData;
