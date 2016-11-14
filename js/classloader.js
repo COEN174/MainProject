62ec8976476a2608c74062f79d12ead4c9a7245f
Jack Kingsman <jack.kingsman@gmail.com>
2016-11-14 13:07:41 -0800

    Fix data population issue with a fresh session. Closes #58

--- js/classloader.js
+++ js/classloader.js
@@ -105,7 +105,10 @@ function loadRequirements() {
 
             if (localStorage.getItem('requirements') !== null) {
                 restoreStatus();
+            } else {
+              saveStatus();
             }
+
             drawCalendar();
             colorCode();
             updateCompletionPercentage();
