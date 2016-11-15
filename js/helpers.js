a84c2d28f85fda88bf07b636109c2f0979ec6385
Pillager225 <ryanloringcooper@gmail.com>
2016-11-14 20:51:06 -0800

    cannot put multiple of the same EE into the EE category

--- js/helpers.js
+++ js/helpers.js
@@ -367,6 +367,15 @@ function hasSatisfaction(req, c) {
     }
 }
 
+function isInEducationalEnrichment(c, eduEnr) {
+	for(i = 0; i < eduEnr.length; i++) {
+		if(eduEnr[i].name == c) {
+			return true
+		}
+	}
+	return false;
+} 
+
 function putInEducationalEnrichment(c) {
     var eeEntry = {};
     eeEntry.name = c;
@@ -380,8 +389,10 @@ function putInEducationalEnrichment(c) {
         // we've already defined the eduEnr as an empty array which is fine
     }
 
-    eduEnr.push(eeEntry);
-    window.localStorage.setItem("educational_enrichment", JSON.stringify(eduEnr));
+	if(!isInEducationalEnrichment(c, eduEnr)) {
+	    eduEnr.push(eeEntry);
+	    window.localStorage.setItem("educational_enrichment", JSON.stringify(eduEnr));
+	}
 }
 
 function satisfyReqInLocalStorage(req, c) {
