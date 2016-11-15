4115371a55cfa6a383ab422a46ae23fc5be3134c
Jack Kingsman <jack.kingsman@gmail.com>
2016-11-15 09:45:03 -0800

    Handle invalid classes. Closes #80

--- js/helpers.js
+++ js/helpers.js
@@ -267,6 +267,11 @@ function getClasses(inputString) {
     var regex = /([a-z][a-z][a-z][a-z])\s*(\d{1,3})/gi;
     var matches = inputString.match(regex);
 
+    if (!matches) {
+        // not a valid class
+        return [];
+    }
+
     var classes = matches.map(function(classEntry) {
         // regex to break out the classes
         var regex = /([a-z][a-z][a-z][a-z])\s*(\d{1,3})/gi;
@@ -306,6 +311,11 @@ function onTextAreaChange(event) {
 function updateColumnsWithTextArea() {
     var rawClass = $('#classInput').val();
     var classes = getClasses(rawClass);
+
+    if(classes.length === 0){
+      return;
+    }
+
     classes.forEach(function(className) {
         setRequirementFromClass(className);
     });
